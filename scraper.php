61c0c7fa5dbb537f5d74febbb78ff4ef2f311663
HeliTeknik <HeliTeknik@users.noreply.github.com>
2017-02-15 14:41:34 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,10 +1,4 @@
 <?php
-require 'scraperwiki.php';
-    require 'scraperwiki/simple_html_dom.php';
-?>
-
-
-<?php
 $url="http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm";
 $html = scraperWiki::scrape( $url );
 $dom = new simple_html_dom();
